e4109a0f21b96dcbf9526873f9ec7c961de0059b
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2023-12-10 02:47:35 +0100

    added changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,6 @@ import "primereact/resources/themes/lara-light-cyan/theme.css";
 import "/node_modules/primeflex/primeflex.css"
 import './App.css'
 import Header from "./Header";
-
-import Maze from "./Maze"
 import Game from "./Game"
 
 export default function App() {
--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -3,17 +3,22 @@ import Maze from './Maze'
 import Logs from './Logs'
 
 function Game() {
+    const [msgs, setMsgs] = useState([])
+    const addMsg = (msg) => {
+        setMsgs([...msgs, msg])
+    }
+
     return (
         <>
             <div className='grid p-6'>
                 <div className="col">
-                    <Maze />
+                    <Maze addMsg={addMsg} />
                 </div>
                 <div className="col">
                     <div className="text-center p-3 border-round-sm bg-primary font-bold ">2</div>
                 </div>
-                <div className="col h-full">
-                    <Logs />
+                <div className="col">
+                    <Logs msgs={msgs} />
                 </div>
             </div>
 
